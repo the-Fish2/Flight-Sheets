670535a3e757b76a37d9aa25787faac3f531a697
the-Fish2 <97804639+the-Fish2@users.noreply.github.com>
2022-10-20 20:06:50 -0700

    Update App.js

--- App.js
+++ App.js
@@ -1,35 +1,20 @@
 import { useState } from 'react'
 
 
-const App = () => {
+const App = (props) => {
 
-  const now = 0 //new Date()
-
-  const [flights, setFlights] = useState(
-    {
-      number: 0,
-      date: now,
-      temperature: 0,
-      mass: 0
-    }
-  )
-  const plusMass = () => {
-    const newFlights = { 
-      ...flights,
-      mass: flights.mass + 1, 
-    }
-    setFlights(newFlights)
-  }
+  const { flights } = props
 
   return (
     <div>
-      <div>
-        {flights.number}
-        {flights.date.toString()}
-        {flights.temperature}
-        {flights.mass}
-        <button onClick={plusMass}>mass</button>
-      </div>
+      <h1>Flights</h1>
+      <ul>
+        {flights.map(flight => 
+          <li key={flight.id}>
+            {flight.content}
+          </li>
+        )}
+      </ul>
     </div>
   )
 }
